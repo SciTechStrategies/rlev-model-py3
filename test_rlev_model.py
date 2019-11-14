6244388d387da2b6164480ddcdaa299da017cd14
Mike Patek <mpatek@gmail.com>
2019-11-13 21:32:33 -0800

    Add Conda environment spec

--- test_rlev_model.py
+++ test_rlev_model.py
@@ -6,6 +6,22 @@ from click.testing import CliRunner
 from rlev_model import cli
 
 
+def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
+    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
+
+def compare_rows(r1, r2):
+    for f1, f2 in zip(r1, r2):
+        assert isclose(f1, f2)
+
+
+def parse_output(output):
+    return [
+        list(map(float, row.split()))
+        for row in output.split('\n')
+    ]
+
+
 class TestCli(unittest.TestCase):
     def test_cli(self):
         runner = CliRunner()
@@ -15,8 +31,10 @@ class TestCli(unittest.TestCase):
 
         output_filename = os.path.join('data', 'sample-output.txt')
         with open(output_filename) as fp:
-            expected_output = fp.read()
-        assert result.output == expected_output
+            expected_output = parse_output(fp.read())
+        result_output = parse_output(result.output)
+        for result_row, expected_row in zip(result_output, expected_output):
+            compare_rows(result_row, expected_row)
 
 
 if __name__ == '__main__':
